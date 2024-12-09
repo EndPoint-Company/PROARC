d928bc5a964e3d8959a03a47f13a502ba36544fa
Mykael A. <mykaelalvesnogueira1@gmail.com>
2024-12-09 20:35:35 -0300

    add missing controls

---
+++ PROARC/src/Control/FileNetworkControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROARC.src.Control
+{
+    public class FileNetworkControl
+    {
+        // Classe pra gerenciar a transferência de arquivos entre Windows (cliente) e Linux (server)
+        // Possível utilização de Samba no server side.
+    }
+}
---
+++ PROARC/src/Control/ProcessoAdministrativoControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROARC.src.Control
+{
+    public class ProcessoAdministrativoControl
+    {
+        // Engloba o controle das entidades-arquivo.
+    }
+}
