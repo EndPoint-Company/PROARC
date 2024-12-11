0b1d10173bb5fab1808faa051beddb7b52ab23d7
Mykael A. <mykaelalvesnogueira1@gmail.com>
2024-12-11 19:07:16 -0300

    add unit tests solution

---
+++ PROARCTests/src/Control/Database/DatabaseOperationsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PROARC.src.Control.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROARC.src.Control.Database.Tests
+{
+    [TestClass()]
+    public class DatabaseOperationsTests
+    {
+        [TestMethod()]
+        public void QuerySqlCommandTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void CreateProgramDatabaseTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
