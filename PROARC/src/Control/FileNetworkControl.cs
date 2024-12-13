0fafa658dc0413ddafb4929761089faf0c229daa
Mykael A. <mykaelalvesnogueira1@gmail.com>
2024-12-13 12:45:48 -0300

    update FileNetworkControl.cs | create tests for files

--- PROARC/src/Control/FileNetworkControl.cs
+++ PROARC/src/Control/FileNetworkControl.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PROARC.src.Models.Arquivos;
 
 namespace PROARC.src.Control
 {
@@ -10,11 +12,39 @@ namespace PROARC.src.Control
     {
         // Classe pra gerenciar a transferência de arquivos entre Windows (cliente) e Linux (server)
         // Possível utilização de Samba no server side.
-        public static void SetLocalDefaultFolder(string path)
+
+        // Localmente
+        // Formato dos caminhos (ex.) -> C:/Users/Documentos/Proarc/
+        private static string? local_DefaultPath = Local_GetDefaultFolder();
+
+        public static void Local_SetDefaultFolder(string path)
+        {
+            // TODO: colocar path na database
+            // Tabela -> local_path varchar
+            //           remote_path varchar
+
+            local_DefaultPath = path;
+        }
+
+        public static string? Local_GetDefaultFolder()
+        {
+            // Pegar caminho na database
+            return null;
+        }
+
+        public static void Local_CriarFolderProcessoAdministrativo(ProcessoAdministrativo processo)
+        {
+            Directory.CreateDirectory($"{local_DefaultPath}/{processo.NumeroProcesso}");
+        }
+
+        public static void Local_CriarDiretorio(Diretorio dir)
         {
 
         }
 
+        public static void Local_AdicionarAquivoToDiretorio(Diretorio dir, Arquivo arquivo)
+        {
 
+        }
     }
 }
---
+++ PROARCTests/src/Control/FileNetworkControlTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PROARC.src.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROARC.src.Control.Tests
+{
+    [TestClass()]
+    public class FileNetworkControlTests
+    {
+        [TestMethod()]
+        public void Local_CriarFolderProcessoAdministrativoTest()
+        {
+            FileNetworkControl.Local_SetDefaultFolder(@"D:/ProarcFiles/Teste1");
+            FileNetworkControl.Local_CriarFolderProcessoAdministrativo(new("","0001ooo2024",2024));
+        }
+    }
+}
