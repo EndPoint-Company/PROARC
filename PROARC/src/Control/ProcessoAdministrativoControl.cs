23e5861ef42b5f6add0355856139f2845a73ae48
Mykael A. <mykaelalvesnogueira1@gmail.com>
2025-01-02 20:46:51 -0300

    fix bug em add processo

--- PROARC/src/Control/ProcessoAdministrativoControl.cs
+++ PROARC/src/Control/ProcessoAdministrativoControl.cs
@@ -23,9 +23,9 @@ namespace PROARC.src.Control
             int? reclamante_id = ReclamanteControl.GetReclamanteId(processo.Reclamante.Rg);
             int? reclamado_id = null; // TODO
 
-            if (processo.Reclamado.Cpf != null)
+            if (processo.Reclamado?.Cpf != null)
                 reclamado_id = ReclamadoControl.GetReclamadoId(processo.Reclamado.Cpf, processo.Reclamado.Nome);
-            if (processo.Reclamado.Cnpj != null)
+            if (processo.Reclamado?.Cnpj != null)
                 reclamado_id = ReclamadoControl.GetReclamadoId(processo.Reclamado.Cnpj, processo.Reclamado.Nome);
 
             AdicionarProcessoAdministrativo(processo.Titulo, processo.Ano, motivo_id, reclamante_id,
@@ -52,20 +52,20 @@ namespace PROARC.src.Control
 
         // Baixo nível, deve ser usada no RegistrarProcessoAdministrativo()
         private static void AdicionarProcessoAdministrativo
-            (string numero_processo, short ano, int? motivo_id = null, int? reclamante_id = null,
+            (string titulo_processo, short ano, int? motivo_id = null, int? reclamante_id = null,
             int? reclamado_id = null, string? caminho_processo = null, DateTime? data_audiencia = null,
             Status status = Status.EmTramitacaoAguardandoEnvioDaNotificacao) // Transformar em objeto depois
         {
             string? sqlFormattedDate = data_audiencia?.ToString("yyyy-MM-dd HH:mm:ss.fff");
 
             string sql = "USE ProArc; INSERT INTO ProcessosAdministrativos " +
-                "(motivo_id, reclamante_id, reclamado_id, numero_processo, status_processo, caminho_processo, ano, data_audiencia)" +
-                $"VALUES ({motivo_id}, {reclamante_id}, {reclamado_id}, '{numero_processo}', '{status}', '{caminho_processo}', {ano}, '{sqlFormattedDate}')";
+                "(motivo_id, reclamante_id, reclamado_id, titulo_processo, status_processo, caminho_processo, ano, data_audiencia)" +
+                $"VALUES ({motivo_id}, {reclamante_id}, {reclamado_id}, '{titulo_processo}', '{status}', '{caminho_processo}', {ano}, '{sqlFormattedDate}')";
 
             DatabaseOperations.QuerySqlCommandNoReturn(sql);
 
-            FileNetworkControl.Local_CriarProcessoAdministrativo(numero_processo);
-            FileNetworkControl.Local_CriarAllDiretorios(numero_processo);
+            FileNetworkControl.Local_CriarProcessoAdministrativo(titulo_processo);
+            FileNetworkControl.Local_CriarAllDiretorios(titulo_processo);
         }
     }
 }
