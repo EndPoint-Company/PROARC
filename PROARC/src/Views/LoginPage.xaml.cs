e282f293a0e47b7b97acff7cc973c4907cedadde
Mykael A. <mykaelalvesnogueira1@gmail.com>
2025-01-18 13:43:59 -0300

    login

--- PROARC/src/Views/LoginPage.xaml.cs
+++ PROARC/src/Views/LoginPage.xaml.cs
@@ -1,5 +1,18 @@
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
 
 namespace PROARC.src.Views
 {
@@ -11,9 +24,63 @@ namespace PROARC.src.Views
             this.RequestedTheme = Microsoft.UI.Xaml.ElementTheme.Light;
         }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(HomeNavigationPage));
+            bool senhaValida = await LoginConnect(CaixaSenha.AccessKey);
+
+            if (senhaValida)
+                Frame.Navigate(typeof(HomeNavigationPage));
+            else 
+                Console.WriteLine("Senha inválida");
+        }
+
+        private async Task<bool> LoginConnect(string password)
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Parse("35.247.235.49"), 9999);
+
+            using TcpClient client = new();
+            await client.ConnectAsync(ipEndPoint);
+
+            NetworkStream stream = client.GetStream();
+
+            client.Client.Send(Encoding.UTF8.GetBytes("AUTH"));
+
+            byte[] buffer = new byte[4096];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine(response);
+            System.Diagnostics.Debug.WriteLine(response);
+
+            var tableData = JsonSerializer.Deserialize<List<object[]>>(response);
+
+            List<String> list = new List<string>();
+
+            foreach (var item in tableData)
+            {
+                list.Add(item[0].ToString());
+            }
+
+            foreach (var salt in list)
+            {
+                using SHA256 sha256Hash = SHA256.Create();
+                byte[] passwordHash = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password + salt));
+
+                client.Client.Send(passwordHash);
+
+                byte[] buffera = new byte[4096];
+                int bytesReada = await stream.ReadAsync(buffera, 0, buffera.Length);
+                string responsee = Encoding.UTF8.GetString(buffera, 0, bytesReada);
+
+                System.Diagnostics.Debug.WriteLine(responsee);
+                Console.WriteLine(responsee);
+
+                if (responsee == "OK")
+                {
+                    return true;
+                }
+            }
+
+            return true;
         }
 
         //private void LoginButton_Click(object sender, RoutedEventArgs e)
