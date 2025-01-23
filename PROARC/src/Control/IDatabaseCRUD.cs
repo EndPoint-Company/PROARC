29813be4ea36c0da22afff1b79107faa0a5faf4e
Mykael A. <mykaelalvesnogueira1@gmail.com>
2025-01-23 20:56:55 -0300

    add interface para database crud

---
+++ PROARC/src/Control/IDatabaseCRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROARC.src.Control
+{
+    public interface IDatabaseCRUD<T>
+    {
+        static async Task<List<T>?> GetAll() { await Task.CompletedTask; throw new NotImplementedException(); }
+        static async Task<T?> Get(int id) { await Task.CompletedTask; throw new NotImplementedException(); }
+        static async Task<bool> Insert(T obj) { await Task.CompletedTask; throw new NotImplementedException(); }
+        static async Task<bool> Update(T obj) { await Task.CompletedTask; throw new NotImplementedException(); }
+        static async Task<bool> Delete(int id) { await Task.CompletedTask; throw new NotImplementedException(); }
+    }
+}
--- PROARC/src/Control/ProcessoAdministrativoControl.cs
+++ PROARC/src/Control/ProcessoAdministrativoControl.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
 using PROARC.src.Models.Arquivos;
 using PROARC.src.Models.Tipos;
+using static PROARC.src.Control.NetworkControl;
 
 namespace PROARC.src.Control
 {
-    public static class ProcessoAdministrativoControl
+    public class ProcessoAdministrativoControl : IDatabaseCRUD<ProcessoAdministrativo>
     {
+        public static async Task<List<ProcessoAdministrativo>?> GetAll()
+        {
+            var request = new { action = "get_all_processos" };
+
+            string response = await SendRequestAsync(request);
+
+            return null;
+        }
+
+        public static async Task<object?> Get(int id)
+        {
+            return null;
+        }
+
+        public static async Task<bool> Insert(object obj)
+        {
+            return false;
+        }
+
+        public static async Task<bool> Update(object obj)
+        {
+            return false;
+        }
+
+        public static async Task<bool> Delete(int id)
+        {
+            return false;
+        }
     }
 }
