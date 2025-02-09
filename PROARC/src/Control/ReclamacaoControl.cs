4585a5ad5b77b03ab0ce70ef4b2f6806cf2f5cb4
Marcos Vitor Souza Freire <115586300+marquinvitor@users.noreply.github.com>
2025-02-09 16:47:45 -0300

    .

---
+++ PROARC/src/Control/ReclamacaoControl.cs
@@ -0,0 +1,64 @@
+﻿using Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static PROARC.src.Control.NetworkControl;
+using PROARC.src.Models;
+using PROARC.src.Models.Arquivos;
+using PROARC.src.Models.Tipos;
+
+namespace PROARC.src.Control
+{
+    public class ReclamacaoControl
+    {
+
+        public static async Task<Reclamacao> GetAsync(string titulo)
+        {
+            var request = new { action = "get_reclamacao_por_titulo", titulo };
+            string response = await SendRequestAsync(request);
+            Console.WriteLine(response);
+            using JsonDocument doc = JsonDocument.Parse(response);
+            var root = doc.RootElement;
+
+            /*
+            if (root.TryGetProperty("reclamacao", out JsonElement motivoElement) && motivoElement.GetArrayLength() == 1)
+            {
+                string nomeMotivo = motivoElement[0].GetString() ?? string.Empty;
+                return new Motivo(nomeMotivo);
+            }
+            */
+
+            return null;
+        }
+
+        public static async Task InsertAsync(List<Reclamado> reclamados)
+        {
+           var request = new { action = "insert_reclamacao", reclamados };
+        }
+
+        public static async Task DeleteAsync(string titulo)
+        {
+            var request = new { action = "delete_reclamacao_por_titulo", titulo };
+            await SendRequestAsync(request);
+        }
+
+        public static async Task<int> CountAsync()
+        {
+            var request = new { action = "count_reclamacoes" };
+            string response =  await SendRequestAsync(request);
+
+            using JsonDocument doc = JsonDocument.Parse(response);
+            var root = doc.RootElement;
+
+            if (root.TryGetProperty("count", out JsonElement countElement))
+            {
+                return countElement.GetInt32();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ PROARCTests/src/Control/ReclamacaoControlTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PROARC.src.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROARC.src.Control.Tests
+{
+    [TestClass()]
+    public class ReclamacaoControlTests
+    {
+        [TestMethod()]
+        public async Task DeleteAsyncTest()
+        {
+            await ReclamacaoControl.DeleteAsync("REC-6717");
+        }
+
+        [TestMethod()]
+        public async Task CountAsyncTest()
+        {
+            Console.WriteLine(await ReclamacaoControl.CountAsync());
+        }
+
+        [TestMethod()]
+        public async Task GetAsyncTest()
+        {
+            Console.WriteLine(await ReclamacaoControl.GetAsync("titulo234"));
+        }
+    }
+}
