27ea6e7e45d0145c96e78098312c5d6f49174425
HenriquePires <henriquepiresh3@gmail.com>
2025-02-20 05:41:58 -0300

    muita coisa

---
+++ PROARC/src/Converters/ReclamacaoConverter.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PROARC.src.Models.Arquivos;
+
+namespace PROARC.src.Converters
+{
+    public static class ReclamacaoConverter
+    {
+        public static string ConverterTitulo(Reclamacao? reclamacao)
+        {
+            return string.IsNullOrEmpty(reclamacao?.Titulo) ? "N/A" : reclamacao.Titulo;
+        }
+
+        public static string ConverterReclamanteNome(Reclamacao? reclamacao)
+        {
+            return string.IsNullOrEmpty(reclamacao?.Reclamante?.Nome) ? "N/A" : reclamacao.Reclamante.Nome;
+        }
+
+        public static string ConverterCpf(Reclamacao? reclamacao)
+        {
+            return string.IsNullOrEmpty(reclamacao?.Reclamante?.CpfFormatado) ? "N/A" : reclamacao.Reclamante.CpfFormatado;
+        }
+
+        public static string ConverterDataAbertura(Reclamacao? reclamacao)
+        {
+            return reclamacao?.DataAbertura != null
+                ? reclamacao.DataAbertura.Value.ToString("dd/MM/yyyy")
+                : "N/A";
+        }
+
+        public static string ConverterStatus(Reclamacao? reclamacao)
+        {
+            return string.IsNullOrEmpty(reclamacao?.Situacao) ? "N/A" : reclamacao.Situacao;
+        }
+
+        public static string ConverterCriador(Reclamacao? reclamacao)
+        {
+            return string.IsNullOrEmpty(reclamacao?.Criador) ? "N/A" : reclamacao.Criador;
+        }
+
+        public static string ConverterDataCriacao(Reclamacao? reclamacao)
+        {
+            return reclamacao?.DataCriacao?.ToString("dd/MM/yyyy HH:mm") ?? "N/A";
+        }
+
+        public static string ConverterCaminhoDir(Reclamacao? reclamacao)
+        {
+            return string.IsNullOrEmpty(reclamacao?.CaminhoDir) ? "N/A" : reclamacao.CaminhoDir;
+        }
+
+        public static string ConverterMotivo(Reclamacao? reclamacao)
+        {
+            return string.IsNullOrEmpty(reclamacao?.Motivo?.Nome) ? "N/A" : reclamacao.Motivo.Nome;
+        }
+
+        public static string ConverterProcurador(Reclamacao? reclamacao)
+        {
+            return string.IsNullOrEmpty(reclamacao?.Procurador?.Nome) ? "N/A" : reclamacao.Procurador.Nome;
+        }
+
+        public static string ConverterReclamado(Reclamacao? reclamacao)
+        {
+            if (reclamacao?.Reclamados == null || reclamacao.Reclamados.Count == 0)
+                return "N/A";
+
+            return reclamacao.Reclamados.First?.Value?.Nome ?? "N/A";
+        }
+
+        public static List<string> ConverterReclamados(Reclamacao? reclamacao)
+        {
+            if (reclamacao?.Reclamados == null || reclamacao.Reclamados.Count == 0)
+                return new List<string> { "N/A" };
+
+            var listaNomes = new List<string>();
+            foreach (var reclamado in reclamacao.Reclamados)
+            {
+                listaNomes.Add(reclamado.Nome ?? "N/A");
+            }
+            return listaNomes;
+        }
+
+        public static string ConverterDataAudiencia(Reclamacao? reclamacao)
+        {
+            if (reclamacao is ReclamacaoGeral reclamacaoGeral)
+            {
+                return reclamacaoGeral.DataAudiencia?.ToString("dd/MM/yyyy HH:mm") ?? "N/A";
+            }
+            return "N/A";
+        }
+
+        public static string ConverterConciliador(Reclamacao? reclamacao)
+        {
+            if (reclamacao is ReclamacaoGeral reclamacaoGeral)
+            {
+                return string.IsNullOrEmpty(reclamacaoGeral.Conciliador) ? "N/A" : reclamacaoGeral.Conciliador;
+            }
+            return "N/A";
+        }
+
+        public static string ConverterAtendente(Reclamacao? reclamacao)
+        {
+            if (reclamacao is ReclamacaoEnel reclamacaoEnel)
+            {
+                return string.IsNullOrEmpty(reclamacaoEnel.Atendente) ? "N/A" : reclamacaoEnel.Atendente;
+            }
+            return "N/A";
+        }
+
+        public static string ConverterContatoEnelTelefone(Reclamacao? reclamacao)
+        {
+            if (reclamacao is ReclamacaoEnel reclamacaoEnel)
+            {
+                return string.IsNullOrEmpty(reclamacaoEnel.ContatoEnelTelefone) ? "N/A" : reclamacaoEnel.ContatoEnelTelefone;
+            }
+            return "N/A";
+        }
+
+        public static string ConverterContatoEnelEmail(Reclamacao? reclamacao)
+        {
+            if (reclamacao is ReclamacaoEnel reclamacaoEnel)
+            {
+                return string.IsNullOrEmpty(reclamacaoEnel.ContatoEnelEmail) ? "N/A" : reclamacaoEnel.ContatoEnelEmail;
+            }
+            return "N/A";
+        }
+
+        public static string ConverterObservacao(Reclamacao? reclamacao)
+        {
+            if (reclamacao is ReclamacaoEnel reclamacaoEnel)
+            {
+                return string.IsNullOrEmpty(reclamacaoEnel.Observacao) ? "N/A" : reclamacaoEnel.Observacao;
+            }
+            return "N/A";
+        }
+
+    }
+}
---
+++ PROARC/src/ViewModels/ReclamacaoViewModel.cs
@@ -0,0 +1,107 @@
+﻿using PROARC.src.Converters;
+using PROARC.src.Models.Arquivos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace PROARC.src.ViewModels
+{
+    public class ReclamacaoViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Reclamacao _reclamacao;
+
+        public string Titulo { get; }
+        public string Reclamante { get; }
+        public string Cpf { get; }
+        public string DataAbertura { get; }
+
+        public List<string> StatusList { get; } = new()
+        {
+            "Aguardando fazer notificação",
+            "Aguardando realização da audiência",
+            "Aguardando resposta da empresa",
+            "Aguardando envio da notificação",
+            "Aguardando documentação",
+            "Atendido",
+            "Não Atendido"
+        };
+
+        private string status;
+        public string Status
+        {
+            get => status;
+            set
+            {
+                if (status != value)
+                {
+                    Debug.WriteLine($"[ViewModel] Status alterado de '{status}' para '{value}'");
+
+                    status = value; // Apenas atualiza a variável local
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        public event System.Action<ReclamacaoViewModel> StatusChanged;
+
+        private void OnStatusChanged()
+        {
+            StatusChanged?.Invoke(this);
+        }
+
+        public string Criador { get; }
+        public string DataCriacao { get; }
+        public string CaminhoDir { get; }
+        public string Motivo { get; }
+        public string Procurador { get; }
+        public string PrimeiroReclamadoNome { get; }
+        public List<string> Reclamados { get; }
+
+        public string DataAudiencia { get; }
+        public string Conciliador { get; }
+        public string Atendente { get; }
+        public string ContatoEnelTelefone { get; }
+        public string ContatoEnelEmail { get; }
+        public string Observacao { get; }
+
+        public ReclamacaoViewModel(Reclamacao reclamacao)
+        {
+            _reclamacao = reclamacao;
+            Titulo = ReclamacaoConverter.ConverterTitulo(reclamacao);
+            Reclamante = ReclamacaoConverter.ConverterReclamanteNome(reclamacao);
+            Cpf = ReclamacaoConverter.ConverterCpf(reclamacao);
+            DataAbertura = ReclamacaoConverter.ConverterDataAbertura(reclamacao);
+            Status = ReclamacaoConverter.ConverterStatus(reclamacao);
+            Criador = ReclamacaoConverter.ConverterCriador(reclamacao);
+            DataCriacao = ReclamacaoConverter.ConverterDataCriacao(reclamacao);
+            CaminhoDir = ReclamacaoConverter.ConverterCaminhoDir(reclamacao);
+            Motivo = ReclamacaoConverter.ConverterMotivo(reclamacao);
+            Procurador = ReclamacaoConverter.ConverterProcurador(reclamacao);
+            PrimeiroReclamadoNome = ReclamacaoConverter.ConverterReclamado(reclamacao);
+            Reclamados = ReclamacaoConverter.ConverterReclamados(reclamacao);
+
+            DataAudiencia = ReclamacaoConverter.ConverterDataAudiencia(reclamacao);
+            Conciliador = ReclamacaoConverter.ConverterConciliador(reclamacao);
+            Atendente = ReclamacaoConverter.ConverterAtendente(reclamacao);
+            ContatoEnelTelefone = ReclamacaoConverter.ConverterContatoEnelTelefone(reclamacao);
+            ContatoEnelEmail = ReclamacaoConverter.ConverterContatoEnelEmail(reclamacao);
+            Observacao = ReclamacaoConverter.ConverterObservacao(reclamacao);
+        }
+
+        public Reclamacao ObterModeloOriginal()
+        {
+            return _reclamacao;
+        }
+    }
+}
